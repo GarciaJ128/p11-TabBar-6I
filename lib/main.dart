ea39babb894b0342d558e1f5a2a0501730be6667
JENNIFER_DENISSE_GARCIA_JOAQUIN <a.21308051280359@cbtis128.edu.mx>
2024-02-29 18:55:11 +0000

    Actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar Jennifer Garcia",
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: MiPaginaInicial(),
     );
   }
-}
+} //Fin AppMiTabBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//Stateful
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} // MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("TabBar Jennifer Garcia"),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(
+              text: "Libro",
+              icon: Icon(Icons.book),
+            ),
+            Tab(
+              text: "Autor",
+              icon: Icon(Icons.person),
+            ),
+            Tab(
+              text: "Carrito",
+              icon: Icon(Icons.shopping_cart),
+            ),
+            Tab(
+              text: "Mi lista",
+              icon: Icon(Icons.list),
+            )
+          ] // Fin de tab
+              ), // Fin bottom TabBar
         ),
-      ),
-    );
-  }
-}
+        body: TabBarView(children: const <Widget>[
+          Center(
+            child: Text(
+              "La metamorfosis",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Franz Kafka",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "El carrito esta vacio",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Libro 1",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+        ] //Jardin de niños
+            ), //Fin de TabBarView
+      ), //Fin Scaffold
+    ); // DefaultTabController
+  } //Fin Widget
+} //_MiPaginaInicialState
